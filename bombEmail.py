4bfb8b9715730a6e4a543643b1bdf1cc728173ac
@Slayerkkk_ <110862456+slayerkk@users.noreply.github.com>
2023-01-23 19:37:38 -0300

    Add files via upload

--- bombEmail.py
+++ bombEmail.py
@@ -3,16 +3,20 @@ import email.message
 
 #email
 def enviar_email():
-    corpo_email = 'teste'
+    corpo_email = 'corpo do email'
     
  
     msg = email.message.Message()
-    msg['Subject'] = 'teste'
-    msg['From'] = 'slayerlindo6@gmail.com'
-    msg['To'] = 'maneosorio55@gmail.com'
-    password = 'qzqfmxnlxaplzbbs'
+    msg['Subject'] = 'titulo do email'
+    msg['From'] = 'coloque o email que voce usara para atacar'
+    msg['To'] = 'coloque o email de qm vc qr atacar'
+    password = 'sua senha de app para email'
     msg.add_header ('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
+
+#video tuto p qm nn sabe criar senha de app
+#https://youtu.be/6o_f_-YMhaU
+
 #login
     s = smtplib.SMTP('smtp.gmail.com: 587')
     s.starttls()
